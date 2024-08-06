0d805ffbb53a1217f1218cafdf630099a1a6e5b7
daymein <daymeing@gmail.com>
2024-08-06 18:19:56 -0500

    Initial Commit

---
+++ AugustProject/AugustProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AugustProjectApp.swift
+//  AugustProject
+//
+//  Created by Daymein Gregorio on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AugustProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
